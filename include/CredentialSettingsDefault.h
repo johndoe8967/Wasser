5096d4890bf0f1d961c1a71aabc6549ee1d6d64c
JOHNDOE8967-PC\johndoe8967 <johndoe8967@gmx.at>
2022-01-04 00:55:28 +0100

    put default credentials in an include file

---
+++ include/CredentialSettingsDefault.h
@@ -0,0 +1,19 @@
+#ifndef STASSID 
+#define STASSID "..."
+#endif
+#ifndef STAPSK
+#define STAPSK "..."
+#endif
+
+#ifndef MQTTHostname
+#define MQTTHostname "mqtt.host.com"
+#endif
+#ifndef MQTTUser
+#define MQTTUser "username"
+#endif
+#ifndef MQTTPassword
+#define MQTTPassword "password"
+#endif
+#ifndef MQTTPort
+#define MQTTPort 1883
+#endif
